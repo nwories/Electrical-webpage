be8343688825b6cedc3089039f4a20deca3b5e64
Ifeanyichukwu <nworieifeanyichukwustephen@gmail.com>
2025-03-17 04:45:05 +0100

    image not displaying corrected

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ const menuIcon_open = document.getElementById('navImg-open');
 const menuIcon_close = document.getElementById('navImg-close');
 
 menuButton.onclick = function () {
-    if(menu.style.right === '-300px') {
-        menu.style.right = '20%'
+    if(menu.style.right === '-500px') {
+        menu.style.right = '25%'
         menuIcon_open.style.display = 'none'
         menuIcon_close.style.display = 'block'
     }else {
-        menu.style.right = '-300px'
+        menu.style.right = '-500px'
         menuIcon_close.style.display = 'none'
         menuIcon_open.style.display = 'block'
     }
